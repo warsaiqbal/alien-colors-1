7eb3fbe7af3be02c47d41fdf5432e356f1e49814
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:20:00 -0700

    Add files via upload

---
+++ 24. Alien Colors #1/app.js
@@ -0,0 +1,10 @@
+//Passes
+var alien_color = "green";
+if (alien_color == 'green') {
+    console.log("You earned 5 points!");
+}
+//Fails
+var alien_color2 = "red";
+if (alien_color2 == 'green') {
+    //condition is false so no output
+}
---
+++ 24. Alien Colors #1/app.ts
@@ -0,0 +1,11 @@
+//Passes
+let alien_color: string = "green"
+if (alien_color == 'green'){
+    console.log("You earned 5 points!");
+}
+
+//Fails
+let alien_color2: string = "red"
+if (alien_color2 == 'green'){
+//condition is false so no output
+}
